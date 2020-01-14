a0cd78508d7745735c55e27e8c097b55b0d1ac13
gregie156 <gregie.beyer@gmail.com>
2020-01-14 12:36:52 +0200

    Update setup_vundle.sh

--- setup_vundle.sh
+++ setup_vundle.sh
@@ -25,21 +25,28 @@ touch ~/.vimrc &&
 
   " The following are examples of different formats supported.
   " Keep Plugin commands between vundle#begin/end.
+  
   " plugin on GitHub repo
+  " https://github.com/tpope/vim-fugitive
   Plugin 'tpope/vim-fugitive'
   " plugin from http://vim-scripts.org/vim/scripts.html
   " Plugin 'L9'
+  
   " Git plugin not hosted on GitHub
+  " https://github.com/wincent/command-t
   Plugin 'git://git.wincent.com/command-t.git'
+  
   " git repos on your local machine (i.e. when working on your own plugin)
-  Plugin 'file:///home/gmarik/path/to/plugin'
+  " Plugin 'file:///home/gmarik/path/to/plugin'
+  
   " The sparkup vim script is in a subdirectory of this repo called vim.
   " Pass the path to set the runtimepath properly.
-  Plugin 'rstacruz/sparkup', {'rtp': 'vim/'}
+  " Plugin 'rstacruz/sparkup', {'rtp': 'vim/'}
   " Install L9 and avoid a Naming conflict if you've already installed a
   " different version somewhere else.
   " Plugin 'ascenator/L9', {'name': 'newL9'}
   
+  " adds ctrl-p and if you have ctags, also shfit-p
   Plugin 'kien/ctrlp.vim'
 
 
